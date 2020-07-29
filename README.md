e92a1e91afd906ecc4a21b116ded33d6927d291b
Jesse Florig <jesseflorig@gmail.com>
2020-07-29 01:38:52 -0400

    update 404s

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ The free and open source manual for cyber security: [https://bible.hackchur.ch](
 
 ## Todo
 
-- [ ] Fix 404
 - [ ] Fix "edit on Github" links (@rocketseat)
 - [ ] Add search
 - [ ] Add custom theme
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from '@rocketseat/gatsby-theme-docs/src/components/Layout';
-import SEO from '@rocketseat/gatsby-theme-docs/src/components/SEO';
+import Layout from "@rocketseat/gatsby-theme-docs/src/components/Layout";
+import SEO from "@rocketseat/gatsby-theme-docs/src/components/SEO";
 
 export default function NotFound() {
   return (
-    <Layout title="Page not found!">
-      <SEO title="404: Not found" />
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Layout title="Whoops, this page doesn't exist yet">
+      <SEO title="404" />
+      <img src="https://media.giphy.com/media/YQitE4YNQNahy/giphy.gif" />
       <p>
-        If you&#39;d like to go back to homepage, <Link to="/">click here</Link>
-        .
+        {"Why don't you "}
+        <a href="https://github.com/Hack-Church/bible-hackchur-ch">write it</a>
+        !?
       </p>
     </Layout>
   );
